110dcf7602b9bd24f024f7b3152a157169cf1449
Manuel Tonneau <manuel.tonneau@mailbox.org>
2020-05-19 08:51:08 +0200

    Add text_length function

--- preprocessing.py
+++ preprocessing.py
@@ -28,10 +28,12 @@ def cleaning(text_str):
     text_str = text_str.strip()
     return text_str
 
+def text_length(text):
+    return len(text.split(' '))
 
 df = pd.read_csv(args.input_file_path, index_col=0, encoding='utf-8', engine='python')
 df.columns = ['text', 'InformationType_label']
 df['ProcessedText'] = df['text'].apply(cleaning)
-df.ProcessedText_length = [len(text.split(' ')) for text in df.ProcessedText]
+df['ProcessedText_length'] = df['ProcessedText'].apply(text_length)
 print(os.path.join(args.output_folder,"preprocessed_" + os.path.split(args.input_file_path)[1]), flush=True)
 df.to_csv(os.path.join(args.output_folder,"preprocessed_" + os.path.split(args.input_file_path)[1]))
